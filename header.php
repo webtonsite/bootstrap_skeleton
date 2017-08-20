adbd5fc305384dd900cc2c68903464dd8be98dbd
webton <vilpilman@gmail.com>
2017-08-20 17:58:13 +0300

    Initial commit

---
+++ header.php
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<!-- bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<?php include_once "navbar.php"; ?>
+<body>	
